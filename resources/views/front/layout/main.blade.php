004e390ba8f15f68f2dcdebcf659972a56354e73
Tiến <letien3413@gmail.com>
2024-10-14 22:01:51 +0700

    add template

---
+++ resources/views/front/layout/main.blade.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    @include('front.layout.head')
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    @include('front.layout.header')
+    <!-- Header End -->
+
+    <!-- Breadcrumb Begin -->
+    @include('front.layout.breadcrumb')
+    <!-- Breadcrumb End -->
+
+    {{-- content --}}
+    @yield('content')
+    {{-- end content --}}
+
+<!-- Footer Section Begin -->
+    @include('front.layout.footer')
+  <!-- Footer Section End -->
+
+  <!-- Search model Begin -->
+  @include('front.layout.search')
+<!-- Search model end -->
+
+<!-- Js Plugins -->
+@include('front.layout.script')
+
+</body>
+
+</html>
