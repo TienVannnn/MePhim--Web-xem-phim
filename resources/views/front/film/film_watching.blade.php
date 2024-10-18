6a72467b553af77802d7564608fd6834eb6793fc
Tiến <letien3413@gmail.com>
2024-10-18 14:52:22 +0700

    add server data film

--- resources/views/front/film/film_watching.blade.php
+++ resources/views/front/film/film_watching.blade.php
@@ -7,8 +7,8 @@
             <div class="col-lg-12">
                 <div class="breadcrumb__links">
                     <a href="/"><i class="fa fa-home"></i> Trang chủ</a>
-                    <a href="{{ route('detail.film', $movie['slug']) }}"> {{ $titleBread }}</a>
-                    <span>{{ $episode['name'] }}</span>
+                    <a href="{{ route('detail.film', $movie['slug']) }}">{{ $titleBread }}</a>
+                    <span>{{ $episode['name'] == 'Full' ? 'Tập Full' : $episode['name'] }}</span>
                 </div>
             </div>
         </div>
@@ -20,21 +20,31 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="anime__video__player">
-                    <iframe src="{{ $episode['link_embed'] }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen width="100%" height="500px"></iframe>
-                    {{-- <video id="player" playsinline controls data-poster="{{ $movie['poster_url'] }}">
-                    </video> --}}
+                    <iframe id="video-iframe" src="{{ $episode['link_embed'] }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen width="100%" height="600px"></iframe>
+                    {{-- <iframe src="{{ $episode['link_embed'] }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen width="100%" height="600px"></iframe> --}}
+                    <div class="d-flex justify-content-center mt-4 server_data p-3">
+                        @foreach ($episodes as $index => $server)
+                            <button class="btn btn-server {{ $index === 0 ? 'active' : '' }}"
+                                    data-server-index="{{ $index }}"
+                                    data-link-server="{{ $server['server_data'][0]['link_embed'] ?? '' }}">
+                                {{ $server['server_name'] }}
+                            </button>
+                        @endforeach
+                    </div>
                 </div>
                 <div class="anime__details__episodes">
                     <div class="section-title">
                         <h5>Danh sách tập</h5>
                     </div>
-                    @foreach ($episodes as $server)
+                @foreach ($episodes as $server)
+                    @if ($loop->first)
                         @foreach ($server['server_data'] as $tap)
                             <a href="{{ route('watching.film', ['slug' => $movie['slug'], 'episodeSlug' => $tap['slug']]) }}" class="{{ $tap['slug'] == $episode['slug'] ? 'active' : '' }}">
                                 {{ $tap['name'] }}
                             </a>
                         @endforeach
-                    @endforeach
+                    @endif
+                @endforeach
                 </div>
             </div>
         </div>
@@ -58,4 +68,21 @@
         </div>
     </div>
 </section>
+
+@section('scripts')
+<script>
+    document.querySelectorAll('.btn-server').forEach(function (button) {
+        button.addEventListener('click', function () {
+            document.querySelectorAll('.btn-server').forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const linkServer = button.getAttribute('data-link-server');
+            const iframe = document.getElementById('video-iframe');
+            if (linkServer) {
+                iframe.src = linkServer;
+            }
+        });
+    });
+</script>
+@endsection
+
 @endsection
--- resources/views/front/layout/main.blade.php
+++ resources/views/front/layout/main.blade.php
@@ -29,7 +29,7 @@
 
 <!-- Js Plugins -->
 @include('front.layout.script')
-
+@yield('scripts');
 </body>
 
 </html>
